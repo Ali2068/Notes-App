32fde27c44cbf261271cc063430922757eb7ba92
Ali2068 <58355367+Ali2068@users.noreply.github.com>
2025-03-02 23:00:24 +0800

    Update note-form.js

--- src/note-form.js
+++ src/note-form.js
@@ -1,8 +1,9 @@
 class NoteForm extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.innerHTML = `
+
+        // Template form
+        this.innerHTML = `
             <style>
                 form { display: flex; flex-direction: column; }
             </style>
@@ -10,7 +11,25 @@ class NoteForm extends HTMLElement {
                 <input id="title" placeholder="Title" required>
                 <textarea id="body" placeholder="Body" required></textarea>
                 <button type="submit">Add Note</button>
-            </form>`;
+            </form>
+        `;
+
+        // Event listener untuk menangani submit form
+        this.querySelector("#note-form").addEventListener("submit", (event) => {
+            event.preventDefault();
+            const title = this.querySelector("#title").value;
+            const body = this.querySelector("#body").value;
+
+            if (title && body) {
+                document.dispatchEvent(new CustomEvent("note-added", {
+                    detail: { title, body }
+                }));
+
+                this.querySelector("#title").value = "";
+                this.querySelector("#body").value = "";
+            }
+        });
     }
 }
+
 customElements.define('note-form', NoteForm);
